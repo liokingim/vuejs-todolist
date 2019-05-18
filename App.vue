d38be86791ecfab69b6a354e3dee0c1a8acfa059
liokingim <liokingim@daum.net>
2019-05-19 03:53:05 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HomeComponent/>
+</template>
+
+<script>
+import HomeComponent from './components/Home'
+export default {
+  name: 'App',
+  components: {
+    HomeComponent
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
